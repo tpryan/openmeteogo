9a67b414d2bf6b5de48a3d93c7070e17fd9c99e7
Terrence Ryan <tpryan@google.com>
2025-07-24 11:24:22 -0700

    chore: implemeting core structure

--- openmateogo.go
+++ openmateogo.go
@@ -1,6 +1,9 @@
 package openmateogo
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 const baseHost = "api.open-meteo.com/v1/forecast"
 const histHost = "archive-api.open-meteo.com/v1/archive"
@@ -8,10 +11,36 @@ const apiPrefix = "server-"
 
 const DefaultUserAgent = "OpenMeteoGo-Client"
 
+const TempUnitCelsius = "celsius"
+const TempUnitFahrenheit = "fahrenheit"
+
+const WindSpeedUnitKMH = "kmh"
+const WindSpeedUnitMS = "ms"
+const WindSpeedUnitMPH = "mph"
+const WindspeedUnitKN = "kn"
+
+const PrecipitationUnitMM = "mm"
+const PrecipitationUnitIN = "in"
+
 type Client struct {
 	apiKey     string
 	UserAgent  string
 	HTTPClient *http.Client
+}
+
+type Options struct {
+	Latitude          float64
+	Longitude         float64
+	TemperatureUnit   string        // Default "celsius"
+	WindspeedUnit     string        // Default "kmh",
+	PrecipitationUnit string        // Default "mm"
+	Timezone          time.Location // Default "UTC"
+	PastDays          int           // Default 0
+	ForcastDays       int
+	Start             time.Time
+	End               time.Time
+	// HourlyMetrics     []string // Lists required hourly metrics, see https://open-meteo.com/en/docs for valid metrics
+	// DailyMetrics      []string // Lists required daily metrics, see https://open-meteo.com/en/docs for valid metrics
 }
 
 func New() *Client {
@@ -27,5 +56,8 @@ func NewWithKey(key string) *Client {
 		HTTPClient: http.DefaultClient,
 		UserAgent:  DefaultUserAgent,
 	}
+}
 
+func (c *Client) url(o *Options) string {
+	return ""
 }
